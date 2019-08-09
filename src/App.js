e05c779c9089a7e82efc53dc03e4fddb9eba2780
Richard <22475@ufp.edu.pt>
2019-08-09 15:00:58 +0100

    testing component Titles render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Titles from "./components/Titles";
 
 class App extends React.Component{
   render(){
     return (
-      <div>Hello World!</div>
+      <div>
+        <Titles/>
+      </div>
     );
   }
 };

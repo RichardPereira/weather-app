6a9403134e2d74fe79d1dd1ab5743680369da237
Richard <22475@ufp.edu.pt>
2019-08-09 17:01:30 +0100

    Definition of the states for the component weather

--- src/App.js
+++ src/App.js
@@ -2,23 +2,52 @@ import React from 'react';
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from './components/Weather';
-import { async } from 'q';
 
-const API_KEY = "b3cc600f6df91bfdae0a91178d64ef0d";
+const API_KEY = "2adda8ae9da1f19336947d22e4351cb0";
 
 
 class App extends React.Component{
-  getWeather = async () =>{
-    const API_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=Porto&appid=${API_KEY}`);
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity:undefined,
+    description:undefined,
+    error: undefined
+  }
+  getWeather = async (event) =>{
+    event.preventDefault();
+
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+
+    const API_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await API_call.json();
     console.log(data);
+
+    this.setState({
+      temperature : data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+
+    });
   }
   render(){
     return (
       <div>
         <Titles/>
-        <Form/>
-        <Weather/>
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }

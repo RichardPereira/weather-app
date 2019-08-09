6f8daa96897452c580fbf35cdbbec6fefa517543
Richard <22475@ufp.edu.pt>
2019-08-09 15:47:55 +0100

    API OpenWeather call setting

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from './components/Weather';
+import { async } from 'q';
+
+const API_KEY = "b3cc600f6df91bfdae0a91178d64ef0d";
+
 
 class App extends React.Component{
+  getWeather = async () =>{
+    const API_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=Porto&appid=${API_KEY}`);
+    const data = await API_call.json();
+    console.log(data);
+  }
   render(){
     return (
       <div>

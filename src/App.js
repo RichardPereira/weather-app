765309d6c7e88191429fc3901fb6b14282c25384
Richard <22475@ufp.edu.pt>
2019-08-09 17:15:34 +0100

    Validation after the call to the API

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Weather from './components/Weather';
 const API_KEY = "2adda8ae9da1f19336947d22e4351cb0";
 
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     temperature: undefined,
     city: undefined,
     country: undefined,
-    humidity:undefined,
-    description:undefined,
+    humidity: undefined,
+    description: undefined,
     error: undefined
   }
-  getWeather = async (event) =>{
+  getWeather = async (event) => {
     event.preventDefault();
 
     const city = event.target.elements.city.value;
@@ -23,24 +23,33 @@ class App extends React.Component{
 
     const API_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await API_call.json();
-    console.log(data);
-
-    this.setState({
-      temperature : data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-
-    });
+    if (city && country) { // Change the state of the elements only is user input a city and a country else do nothing 
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a City and Country"
+      });
+    }
   }
-  render(){
+  render() {
     return (
       <div>
-        <Titles/>
-        <Form getWeather={this.getWeather}/>
-        <Weather 
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather
           temperature={this.state.temperature}
           city={this.state.city}
           country={this.state.country}
